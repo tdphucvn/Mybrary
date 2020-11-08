dd2b4b03ffcf163ee2a92705a94467834ee151d0
Philip Tran <tran.philip02@gmail.com>
2020-11-08 16:17:34 +0100

    Initial Author Routes

--- server.js
+++ server.js
@@ -6,19 +6,26 @@ const express = require('express');
 const app = express();
 const expressLayouts = require ('express-ejs-layouts');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+
+//app.use(express.json());
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}));
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
+
 
 try{
-    mongoose.connect(process.env.DB_CONNECTION, {useUnifiedTopology: true , useNewUrlParser: true});
+    mongoose.connect(process.env.DB_CONNECTION, {useUnifiedTopology: true , useNewUrlParser: true}, () => console.log('Success Connection'));
 }catch (err){
     console.log('Access denied');
 };
