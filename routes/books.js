44b377874c686f7122493d8fcdd90c5092ffb8ad
Philip Tran <tran.philip02@gmail.com>
2020-11-14 11:21:20 +0100

    Backend Finshed

--- routes/books.js
+++ routes/books.js
@@ -54,13 +54,85 @@ router.post('/', async (req, res) => {
     try{
         //saving the object to database
         const newBook = await book.save();
-        res.redirect('books');
+        res.redirect(`/books/${newBook.id}`);
     } catch {
         renderNewPage(res, book, true);
     }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id).populate('author')
+        .exec();
+        res.render('books/show', {book: book});
+    } catch {
+        res.redirect('/');
+    };
+})
+
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id);
+        renderEditPage(res, book);
+    } catch {
+        res.redirect('/');
+    };
+});
+
+
+router.put('/:id', async (req, res) => {
+    let book;
+
+    try{
+        book = await Book.findById(req.params.id);
+        book.title = req.body.title;
+        book.author = req.body.author;
+        book.publishDate = new Date(req.body.publishDate);
+        book.pageCount = req.body.pageCount;
+        book.description = req.body.description;
+        if (req.body.cover != null && req.body.cover !== ''){
+            saveCover(book. req.body.cover);
+        };
+        await book.save();
+        res.redirect(`/books/${book.id}`);
+    } catch {
+        if (book != null){
+            renderNewPage(res, book, true);
+        }else{
+            res.redirect('/');
+        };
+    }
+});
+
+
+router.delete('/:id', async (req, res) => {
+    let book;
+    try {
+        book = await Book.findById(req.params.id);
+        await book.remove();
+        res.redirect('/');
+    }catch {
+        if (book != null){
+            res.render('books/show', {
+                book: book,
+                errorMessage: 'Could not remove book'
+            });
+        }else{
+            res.redirect('/');
+        };
+    };
+});
+
+
 async function renderNewPage(res, book, hasError = false){
+    renderFormPage(res, book, 'new', hasError);
+};
+
+async function renderEditPage(res, book, hasError = false){
+    renderFormPage(res, book, 'edit', hasError);
+};
+
+async function renderFormPage(res, book, form, hasError = false){
     try{
         //load the list of authors from database
         const authors = await Author.find({});
@@ -69,12 +141,19 @@ async function renderNewPage(res, book, hasError = false){
             authors: authors,
             book: book
         }
-        if (hasError) params.errorMessage = 'Error Creating Book';
-        res.render('books/new', params);
+        if (hasError){
+            if(form === 'edit'){
+                params.errorMessage = 'Error Updating Book';
+            }else{
+                params.errorMessage = 'Error Creating Book';
+            }
+        }
+        res.render(`books/${form}`, params);
     } catch{
         res.redirect('/authors');
     }
 }
+
 
 
 function saveCover(book, coverEncoded){
